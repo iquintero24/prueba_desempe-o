63d5c9974f73d7508fb6166a3e2f7d40e8c1d96a
Isaac David Quintero Lobo <isaacquintero4k@gmail.com>
2025-07-14 14:06:33 -0500

    🛣️  feat: creating the routes.js file

---
+++ app/public/js/route.js
@@ -0,0 +1,4 @@
+/* Instructions: Implement the routing and view logic by importing the necessary functions from views.js and auth.js.
+You can add, modify, or remove routes as needed for your application.*/
+
+
